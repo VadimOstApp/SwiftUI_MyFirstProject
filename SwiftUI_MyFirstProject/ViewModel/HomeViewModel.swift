9bcc89ef9af92b33e4989b630e1371f144f70a11
Vadim_Ost <apple.ostashkevich@gmail.com>
2022-09-02 10:34:32 +0300

    add products image

--- SwiftUI_MyFirstProject/ViewModel/HomeViewModel.swift
+++ SwiftUI_MyFirstProject/ViewModel/HomeViewModel.swift
@@ -13,13 +13,13 @@ class HomeViewModel: ObservableObject {
     
     // Sample product...
     @Published var products: [Product] = [
-        Product(type: .Wearable, title: "Apple Watch", subtitle: "Series 6: Red", price: "$359", productImage: "AppleWatch6"),
-        Product(type: .Wearable, title: "Samsung Watch", subtitle: "Gear: Black", price: "$180", productImage: "AppleWatch6"),
-        Product(type: .Wearable, title: "Apple Watch", subtitle: "Series 4: Black", price: "$250", productImage: "AppleWatch6"),
-        Product(type: .Phones, title: "iPhone 13", subtitle: "A15 - Pink", price: "$699", productImage: "iPhone13"),
-        Product(type: .Phones, title: "iPhone 12", subtitle: "A14 - Blue", price: "$599", productImage: "iPhone12"),
-        Product(type: .Phones, title: "iPhone 11", subtitle: "A13 - Purple", price: "$499", productImage: "iPhone11"),
-        Product(type: .Phones, title: "iPhone SE 2", subtitle: "A13 - White", price: "$399", productImage: "iPhoneSE"),
+        Product(type: .Wearable, title: "Apple Watch", subtitle: "Series 6: Red", price: "$359", productImage: "AppleWatch6Red"),
+        Product(type: .Wearable, title: "Samsung Watch", subtitle: "Watch4 Classic: Black", price: "$369", productImage: "Samsung"),
+        Product(type: .Wearable, title: "Apple Watch", subtitle: "Series 4: Black", price: "$250", productImage: "AppleWatch4Black"),
+        Product(type: .Phones, title: "iPhone 13", subtitle: "A15 - Pink", price: "$699", productImage: "iPhone13Pink"),
+        Product(type: .Phones, title: "iPhone 12", subtitle: "A14 - Blue", price: "$599", productImage: "iPhone12Blue"),
+        Product(type: .Phones, title: "iPhone 11", subtitle: "A13 - Purple", price: "$499", productImage: "iPhone11Purple"),
+        Product(type: .Phones, title: "iPhone SE 2", subtitle: "A13 - White", price: "$399", productImage: "iPhoneSEWhite"),
         Product(type: .Laptops, title: "MacBook Air", subtitle: "M1 - Gold", price: "$999", productImage: "MacBookAir"),
         Product(type: .Laptops, title: "MacBook Pro", subtitle: "M1 - Space Gray", price: "$1299", productImage: "MacBookPro"),
         Product(type: .Laptops, title: "iMac", subtitle: "M1 - Purple", price: "$1599", productImage: "iMac"),
